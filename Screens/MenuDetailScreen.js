6bd5a92ec2fef21fc645bf3065d1ed59d2573714
Tushar Agrawal <tusharsagrawal16@gmail.com>
2021-07-08 19:58:37 +0530

    added fav button

--- Screens/MenuDetailScreen.js
+++ Screens/MenuDetailScreen.js
@@ -1,42 +1,42 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import {MEAL} from "../Data/DummyData";
-
-
-
-
-
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { MEAL } from "../Data/DummyData";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../Components/CustomHeaderButton"
 const getMeals = (id) => {
-    var myMeal = MEAL.filter(meal => meal.id === id)
-    return myMeal[0];
-  }
+  var myMeal = MEAL.filter((meal) => meal.id === id);
+  return myMeal[0];
+};
 
 const MenuDetailScreen = (props) => {
   const meal = getMeals(props.navigation.getParam("id"));
-    return (
-        <View style={styles.screen}>
-            <Text>{meal.title}</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.screen}>
+      <Text>{meal.title}</Text>
+    </View>
+  );
+};
 
+MenuDetailScreen.navigationOptions = ({ navigation }) => {
+  return {
+    title: navigation.getParam("title"),
+    headerStyle: {
+      backgroundColor: navigation.getParam("categoryColor"),
+    },
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item title="Fav" iconName="ios-star-outline" onPress={()=> console.log("Im favorite")} />
+      </HeaderButtons>
+    ),
+  };
+};
 
-MenuDetailScreen.navigationOptions = ({navigation}) => {
-    return ({
-      title: navigation.getParam('title'),
-      headerStyle: {
-        backgroundColor: navigation.getParam('categoryColor')
-      }
-    })
-    };
-    
-    
+export default MenuDetailScreen;
 
-
-export default MenuDetailScreen
-
-const styles = StyleSheet.create({    screen: {
-    flex:1,
-    justifyContent: 'center',
-    alignItems: 'center'
-}})
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
