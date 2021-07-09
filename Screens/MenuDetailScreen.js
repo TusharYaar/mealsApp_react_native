bb2d05e63d1756d77668c84d6a3d4dcf75d2a0f9
Tushar Agrawal <tusharsagrawal16@gmail.com>
2021-07-09 10:35:21 +0530

    meals screen update

--- Screens/MenuDetailScreen.js
+++ Screens/MenuDetailScreen.js
@@ -1,19 +1,39 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image,ScrollView } from "react-native";
 import { MEAL } from "../Data/DummyData";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../Components/CustomHeaderButton"
 const getMeals = (id) => {
-  var myMeal = MEAL.filter((meal) => meal.id === id);
-  return myMeal[0];
+  var myMeal = MEAL.find((meal) => meal.id === id);
+  return myMeal;
 };
+
+const ListItem =  (props) => {
+  return (
+    <View>
+    <Text>{props.children}</Text>
+    </View>
+  )
+}
 
 const MenuDetailScreen = (props) => {
   const meal = getMeals(props.navigation.getParam("id"));
   return (
-    <View style={styles.screen}>
-      <Text>{meal.title}</Text>
+<ScrollView>
+  <Image source={{uri: meal.imageUrl}} style={styles.image}/>
+    <View style={styles.container}>
+      <Text style={styles.title}>{meal.title}</Text>
+      <View>
+        <Text style={styles.header}>Ingredients</Text>
+        {meal.Ingredients.map((ing,index) => <ListItem key={index}>{ing}</ListItem>)}
+      </View>
+      <View>
+        <Text style={styles.header}>Steps</Text>
+        {meal.steps.map((stp,index) => <ListItem key={`step-${index}`}>{stp}</ListItem>)}
+      </View>
     </View>
+    
+</ScrollView>
   );
 };
 
@@ -34,9 +54,20 @@ MenuDetailScreen.navigationOptions = ({ navigation }) => {
 export default MenuDetailScreen;
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  image: {
+    width: "100%",
+    height: 250,
   },
+  title: {
+    fontSize: 24,
+    fontFamily: "Inter_900Black",
+  },
+  header: {
+    fontSize: 20,
+    fontFamily: "Inter_900Black",
+    marginVertical: 10,
+  },
+  container: {
+    padding: 5,
+  }
 });
